47f7303ea7867233a55eaf63820c328836220149
gary92gs <gary92.gs@gmail.com>
2024-01-19 10:34:27 -0700

    took out 1 console.log

--- index.js
+++ index.js
@@ -31,7 +31,6 @@ function newTask(title, description) {
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box"); // task 0
 const task2 = newTask("Do Laundry", "😨"); // task 1
 const tasks = [task1, task2];
-console.log(tasks);
 
 task1.logState(); // Clean Cat Litter has not been completed
 task1.markCompleted();
